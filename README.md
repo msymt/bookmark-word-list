340df15e112e5b4c48ee95703f69fb61c3b31062
msymt <nam.nam580@gmail.com>
2021-05-16 11:41:32 +0900

    add README script poetry file

---
+++ README.md
@@ -0,0 +1,28 @@
+# bookmark-word-list
+
+Chromeのブックマークのフォルダ内にあるWeblioのサイトから単語名とその意味を取得する
+
+コードの大半は[こちら](https://harigami.net/cd?hsh=c4a5b7ed-8821-4d0d-a60d-e93fa69a9d65#L17)を利用した．
+
+## how to use
+
+以下のコードの数字を，取得したい英単語のフォルダの番号へ入れ替える．
+
+例えば，ブックマークリスト内のフォルダの内，先頭に設置しているなら以下のように`0`と書く．
+
+```Python
+bookmarks = bookmark_data['roots']['bookmark_bar']['children'][0]['children']
+```
+
+その後，以下を実行する
+
+```bash
+poetry install
+poetry run python solve.py
+```
+実行するとcsv形式のファイルが出力される．以下はnovelの場合
+
+```
+,0,1
+0,novel ,(よい意味で)新しい、新奇な、奇抜な
+```
---
+++ bookmark.py
@@ -0,0 +1,51 @@
+#### get bookmark info ####
+import json
+import getpass
+# set a PATH to your bookmark repository
+CHROME_BOOKMARK_PATH = (
+    '/Users/{username}/Library/Application Support/'
+    'Google/Chrome/Default/Bookmarks'
+).format(username=getpass.getuser())
+# get dictionary-type data of your bookmarks
+def get_chrome_bookmark_data() -> dict:
+    with open(CHROME_BOOKMARK_PATH) as f:
+        return json.load(f)
+bookmark_data = get_chrome_bookmark_data()
+
+# locate your wanted bookmarks /////This depends on the structure of your bookmarks.
+# bookmarks = bookmark_data['roots']['bookmark_bar']['children'][6]['children']
+bookmarks = bookmark_data['roots']['bookmark_bar']['children'][0]['children']
+
+#### get the urls of the bookmarks ####
+# define a function that extracts a url from each bookmark
+def get_weblio_url(bookmark):
+    if 'Weblio' in bookmark['name'] and '意味・' in bookmark['name']:
+        return bookmark['url']
+# apply the function to the whole bookmarks
+url_list = list(map(get_weblio_url,bookmarks))
+print(url_list)
+# filter out None from the url list.
+url_list_filtered = list(filter(None, url_list))
+
+#### extract words and meanings from html files ####
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+i = 0
+df = pd.DataFrame()
+while i < len(url_list_filtered):
+    # analyse html files
+    response = requests.get(url_list_filtered[i])
+    soup = BeautifulSoup(response.content, "html.parser")    
+    # extract meanings by setting the class
+    elements = soup.select(".content-explanation.ej")
+    # extract only alphabets from the title, which is the word
+    alphabet = re.compile('[a-z,A-Z]+')
+    word = ' '.join(alphabet.findall(soup.title.text)).replace('Weblio', '')
+    # store the words and meanings in a dataframe
+    if len(elements) > 0:
+        df = df.append([[word, elements[0].text]], ignore_index=True)
+    i += 1
+# export the dataframe as a csv file
+df.to_csv('Weblio_word_list.csv')
